b1aa99417da772ec31a0da40d02bd547b8b95c1b
Shwetabh vikram <vikramshwetabh29@gmail.com>
2025-08-05 19:34:19 +0530

    deleted

--- practic3/3.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	something := make([]int, 8, 10) //first int is len, second is capacity
-	something = append(something, 2)
-	fmt.Println(something, len(something), cap(something))
-}
